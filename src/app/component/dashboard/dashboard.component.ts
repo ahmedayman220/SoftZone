d7cfca5fb237f97c6b9460e01a93f4689b1f72f9
ahmedayman220 <ahmedayman.com220@gmail.com>
2023-07-14 16:42:30 +0300

    ADD COMMENT

--- src/app/component/dashboard/dashboard.component.ts
+++ src/app/component/dashboard/dashboard.component.ts
@@ -1,8 +1,8 @@
-import { EmployeeService } from 'src/app/service/employee.service';
-import { Component } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
-import { OnInit } from '@angular/core';
-import { EmployeeModel } from 'src/app/model/employee.model';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+
+import {EmployeeModel} from 'src/app/model/employee.model';
+import {EmployeeService} from 'src/app/service/employee.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -10,108 +10,96 @@ import { EmployeeModel } from 'src/app/model/employee.model';
   styleUrls: ['./dashboard.component.css']
 })
 
-
 export class DashboardComponent implements OnInit {
-  
   // form Values
-  formValue !: FormGroup;
+  formValue!: FormGroup; // FormGroup for managing form data
 
   // display status for button Add Or edit
-  savingEmployee = false;
-  editButton: boolean = true;
-  addButton: boolean = false;
+  savingEmployee = false; // Flag to indicate if an employee is being saved
+  editButton = true; // Flag to indicate if the edit button is displayed
+  addButton = false; // Flag to indicate if the add button is displayed
 
   // Pagination
-  page: number = 1;
-  count: number = 0;
-  tableSize: number = 10;
-  tableSizes: any = [3, 6, 9, 12];
+  page = 1; // Current page number
+  count = 0; // Total count of employees
+  tableSize = 10; // Number of employees per page
+  tableSizes = [3, 6, 9, 12]; // Options for table size
 
-  
-  constructor(private formBuilder: FormBuilder, private api: EmployeeService) { };
-
-  // ===========================================================================
-  // Start Page
-  // ===========================================================================
+  constructor(private formBuilder: FormBuilder, private api: EmployeeService) {
+  }
 
   ngOnInit(): void {
     // get All data
     this.getAllEmployees();
 
-    // validation & Inetial Data
+    // validation & Initial Data
     this.formValue = this.formBuilder.group({
-      "empName": new FormControl('', [Validators.required]),
-      "empEmail": new FormControl('', [Validators.required, Validators.email]),
-      "empAddress": new FormControl('', [Validators.required]),
-      "empPhone": new FormControl('', [Validators.required, Validators.pattern(/^(011|012|010)\d{8}$/)]),
-    })
+      empName: new FormControl('', [Validators.required]), // FormControl for employee name
+      empEmail: new FormControl('', [Validators.required, Validators.email]), // FormControl for employee email
+      empAddress: new FormControl('', [Validators.required]), // FormControl for employee address
+      empPhone: new FormControl('', [Validators.required, Validators.pattern(/^(011|012|010)\d{8}$/)]) // FormControl for employee phone number
+    });
   }
 
   // ===========================================================================
-  // Push Employee Data from function getAllEmployees()
+  // GET ALL EMPLOYEE
   // ===========================================================================
 
-  employees: any[] = [];
+  employees: any[] = []; // Array to store the retrieved employee data
 
   getAllEmployees() {
     this.api.getAllEmployees().subscribe(
       (response) => {
-        this.employees = response;
+        this.employees = response; // Assign the retrieved employees to the array
       },
       (error) => {
-        console.error(error);
+        console.error(error); // Handle error in retrieving employees
       }
     );
   }
 
-
   // ===========================================================================
-  // Post Employee
+  // ADD EMPLOYEE
   // ===========================================================================
 
-  employeeModelObj: EmployeeModel[] = [];
+  employeeModelObj: EmployeeModel[] = []; // Array to store employee data for posting
+
   postEmployee() {
-    this.savingEmployee = true;
-    this.employeeModelObj = this.formValue.value
+    this.savingEmployee = true; // Set the flag to indicate saving employee
+
+    this.employeeModelObj = this.formValue.value; // Get the employee data from the form
     console.log(this.employeeModelObj);
-    this.api.addEmployee(this.employeeModelObj)
-      .subscribe(
-        response => {
 
-          document.getElementById('cancel')?.click();
-          this.successMessage('Add');
-          this.formValue.reset();
-          this.getAllEmployees()
+    this.api.addEmployee(this.employeeModelObj).subscribe(
+      (response) => {
+        document.getElementById('cancel')?.click(); // Trigger cancel button click event
+        this.successMessage('Add'); // Display success message
+        this.formValue.reset(); // Reset the form
+        this.getAllEmployees(); // Retrieve all employees again
+      },
+      (err) => {
+        alert('Something Went Wrong'); // Handle error in posting employee
+      }
+    );
 
-        },
-        err => {
-          alert('Somthing Went Wrong');
-        }
-      )
-    this.savingEmployee = false;
+    this.savingEmployee = false; // Reset the flag after saving employee
   }
 
-
-  // ===========================================================================
-  // Display button Add
-  // ===========================================================================
-
   displayAddButton() {
-    this.formValue.reset()
-    this.addButton = true;
-    this.editButton = false;
+    this.formValue.reset(); // Reset the form values
+    this.addButton = true; // Show the add button
+    this.editButton = false; // Hide the edit button
   }
 
   handleButtonClick() {
     if (this.editButton) {
-      this.editEmployee();
+      this.editEmployee(); // Call editEmployee function if editButton is true
     } else {
-      this.postEmployee();
+      this.postEmployee(); // Call postEmployee function if editButton is false
     }
   }
-
   // ===========================================================================
-  // Make the value for input have valus of employee that i want to edit it
+  // EDIT EMPLOYEE
   // ===========================================================================
 
   employeeData = {
@@ -123,102 +111,102 @@ export class DashboardComponent implements OnInit {
   };
 
   editFormValue(id: any) {
-
-    this.addButton = false;
-    this.editButton = true;
+    this.addButton = false; // Hide the add button
+    this.editButton = true; // Show the edit button
 
     this.api.getEmployeeById(id).subscribe(
-      res => {
-        this.employeeData = res;
-        this.formValue.patchValue(res);
-      }, err => {
-        console.log(err)
-        alert('Somthing Went Wrong');
+      (res) => {
+        this.employeeData = res; // Assign the retrieved employee data to the object
+        this.formValue.patchValue(res); // Set the form values to the retrieved employee data
+      },
+      (err) => {
+        console.log(err);
+        alert('Something Went Wrong'); // Handle error in retrieving employee by ID
       }
-    )
+    );
   }
 
   editEmployee() {
-    this.savingEmployee = true;
-    this.employeeData = { ...this.employeeData, ...this.formValue.value }
-    this.api.editEmployee(this.employeeData)
-      .subscribe(
-        response => {
-          document.getElementById('cancel')?.click();
-          this.successMessage('edit');
-          this.formValue.reset();
-          this.getAllEmployees()
-        },
-        err => {
-          console.log(err)
-          alert('Somthing Went Wrong');
-        }
-      )
+    this.savingEmployee = true; // Set the flag to indicate saving employee
 
-    this.savingEmployee = false;
-  }
+    this.employeeData = {...this.employeeData, ...this.formValue.value}; // Update employeeData object with form values
 
-
-  // ===========================================================================
-  // Delete Employee
-  // ===========================================================================
-  deleteEmployee(id: number) {
-    console.log('test')
-    this.api.deleteEmployeeById(id).subscribe(
-      res => {
-        console.log("success")
-        this.successMessage('delete');
-      }, err => {
-        console.log(err)
-        alert(err);
+    this.api.editEmployee(this.employeeData).subscribe(
+      (response) => {
+        document.getElementById('cancel')?.click(); // Trigger cancel button click event
+        this.successMessage('edit'); // Display success message
+        this.formValue.reset(); // Reset the form
+        this.getAllEmployees(); // Retrieve all employees again
+      },
+      (err) => {
+        console.log(err);
+        alert('Something Went Wrong'); // Handle error in editing employee
       }
-    )
+    );
+
+    this.savingEmployee = false; // Reset the flag after saving employee
   }
 
   // ===========================================================================
-  // Display The Error Under Input
+  // DELETE EMPLOYEE
   // ===========================================================================
+
+  deleteEmployee(id: number) {
+    this.api.deleteEmployeeById(id).subscribe(
+      (res) => {
+        console.log('success');
+        this.successMessage('delete'); // Display success message for deleting employee
+      },
+      (err) => {
+        console.log(err);
+        alert(err); // Handle error in deleting employee
+      }
+    );
+  }
+
+  // ===========================================================================
+  // DISPLAY ERROR BEFORE INPUT
+  // ===========================================================================
+
   isFieldInvalid(field: string) {
     const control = this.formValue.get(field);
-    return control !== null && control.touched && control.invalid;
+    return control !== null && control.touched && control.invalid; // Check if the form field is invalid
   }
 
-
   // ===========================================================================
-  // Display Success Massege For Adding Employee
+  // DISPLAY SUCCESS AFTER DO ( ADD , EDIT , DELETE )
   // ===========================================================================
 
-  display = false;
-  message = '';
+  display = false; // Flag to control the display of success message
+  message = ''; // Success message
+
   successMessage(proccess: string) {
-    this.display = true;
-    if (proccess == 'Add') {
-      this.message = 'Added'
-    } else if ('Edit') {
-      this.message = 'Edited'
+    this.display = true; // Show the success message
+    if (proccess === 'Add') {
+      this.message = 'Added'; // Set success message for adding employee
+    } else if (proccess === 'Edit') {
+      this.message = 'Edited'; // Set success message for editing employee
     } else {
-      this.message = 'Deleted'
+      this.message = 'Deleted'; // Set success message for deleting employee
     }
+
     setTimeout(() => {
-      this.display = false;
+      this.display = false; // Hide the success message after 5 seconds
     }, 5000);
   }
 
   // ===========================================================================
-  // Pagination
+  // PAGENATION
   // ===========================================================================
 
   onTableDataChange(event: any) {
-    this.page = event;
-    this.getAllEmployees();
+    this.page = event; // Update the current page number
+    this.getAllEmployees(); // Retrieve all employees again
   }
+
   onTableSizeChange(event: any): void {
-    this.tableSize = event.target.value;
-    this.page = 1;
-    this.getAllEmployees();
+    this.tableSize = event.target.value; // Update the number of employees per page
+    this.page = 1; // Reset the current page number
+    this.getAllEmployees(); // Retrieve all employees again
   }
-
-
 }
-
-
